3b870b0b2c13db2a1bbc370320f40f6ede659304
BlackLotus <arp930507@outlook.com>
2021-05-03 13:45:08 -0500

    Commit inicial, por problemas con mi github vuelvo a subir pero en otra liga

---
+++ models/Cliente.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const ClientesSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    apellido: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    direccion: {
+        type: String,
+        trim: true
+    }, 
+    ciudad: {
+        type: String,
+        trim: true
+    }, 
+    pais: {
+        type: String,
+        trim: true
+    }, 
+    empresa: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    telefono: {
+        type: String,
+        trim: true
+    },
+    creado: {
+        type: Date,
+        default: Date.now() 
+    }, 
+    vendedor: {
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true,
+        ref: 'Usuario'
+    }
+
+});
+
+module.exports = mongoose.model('Cliente', ClientesSchema);
